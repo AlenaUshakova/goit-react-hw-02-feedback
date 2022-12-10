5510b7205230335a0fe01a4e89a35ddb5686d0ba
AlenaUshakova <alenushakova@gmail.com>
2022-12-10 20:20:33 +0200

    add new files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { FeedbackOptions } from '../FeedbackOptions/FeedbackOptions';
+import { Statistics } from '../Statistics/Statistics';
+import { Section } from '../Section/Section';
+import { Notification } from '../Notification/Notification';
+import { AppBox } from './App.styled';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, num) => acc + num, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return this.countTotalFeedback() === 0
+      ? 0
+      : Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  handleLeaveFeedback = e => {
+    if (e === 'good') {
+      this.setState({ good: this.state.good + 1 });
+    } else if (e === 'neutral') {
+      this.setState({ neutral: this.state.neutral + 1 });
+    } else if (e === 'bad') {
+      this.setState({ bad: this.state.bad + 1 });
+    }
+  };
+
+  render() {
+    return (
+      <AppBox>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.handleLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+      </AppBox>
+    );
+  }
+}
---
+++ src/components/Notification/Notification.styled.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const NotificMessage = styled.h2`
+  color: rgba(250, 43, 7, 0.979);
+  font-size: 2.5rem;
+  font-weight: 400;
+`;
